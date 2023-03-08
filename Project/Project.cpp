dff4b3a8ac98d3caa655270c4fc37429518ac15d
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2023-03-08 15:19:05 +0300

    fix

---
+++ Project/Project.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	char task;
+	int sum, num1, num2;
+
+	cin >> num1;
+	cin >> task;
+	cin >> num2;
+	switch(task){
+		case("+"):
+			sum = num1 + num2;
+		case("-"):
+			sum = num1 - num2;
+		case("*"):
+			sum = num1 * num2;
+	}
+	
+
+	std::cout << sum;
+}
